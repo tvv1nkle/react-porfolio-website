fe72212b34178049bc39df249e6aafae3029872d
Nutchawakorn NaLampang <nutchawakorn.n@gmail.com>
2022-07-26 02:07:10 +0700

    contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,14 @@
+import { FaGithub, FaMailBulk } from "react-icons/fa";
+import classes from './Contact.module.css'
+
 const Contact = () => {
     return ( 
-        <div>
+        <div className={classes.Contact}>
             <h1>Contact</h1>
+            <h2><a href="https://github.com/tvv1nkle" target="_blank" rel="noreferrer">
+                Github
+            </a></h2>
+            <h2>nutchawakorn.n@gmail.com</h2>
         </div>
      );
 }
