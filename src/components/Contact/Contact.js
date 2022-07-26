9bb319ab438299eb4e28c180ecb08ffd7b1e73ef
Nutchawakorn NaLampang <nutchawakorn.n@gmail.com>
2022-07-26 12:14:48 +0700

    contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,13 +1,19 @@
 import { FaGithub, FaMailBulk } from "react-icons/fa";
 import classes from './Contact.module.css'
 
+
+const handleURL = (url) => {
+    return () => window.open(url, "_blank")
+}
+
 const Contact = () => {
     return ( 
         <div className={classes.Contact}>
             <h1>Contact</h1>
-            <h2><a href="https://github.com/tvv1nkle" target="_blank" rel="noreferrer">
-                Github
-            </a></h2>
+            <FaGithub color='black' size='50px' style={{padding: '1%'}}
+            onClick={handleURL('https://github.com/tvv1nkle')} />
+            <FaMailBulk color='black' size='50px' style={{padding: '1%'}}
+            onClick={handleURL('nutchawakorn.n@gmail.com')} />
             <h2>nutchawakorn.n@gmail.com</h2>
         </div>
      );
